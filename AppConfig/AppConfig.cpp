a720a89b64b120302b821b58b88e414d9a5f50aa
Heron Hu <spectre.hhr@outlook.com>
2020-12-12 23:29:32 +0800

    修改为 cmake 项目

--- AppConfig/AppConfig.cpp
+++ AppConfig/AppConfig.cpp
@@ -1,4 +1,3 @@
-#include "pch.h"
 #include "AppConfig.h"
 #include <fstream>
 
@@ -6,47 +5,47 @@ using namespace std;
 using namespace toml;
 using namespace config;
 
-AppConfig::AppConfig(const char* configFileName) : configFileName(configFileName)
+AppConfig::AppConfig(const char *configFileName) : configFileName(configFileName)
 {
-	// 从文件中读取配置文档
-	this->config = parse(configFileName);
+    // 从文件中读取配置文档
+    this->config = parse(configFileName);
 
-	// 判断是否为空文档
-	if (this->config.size() == 0) {
-		// 重置为默认配置
-		resumeDefault();
-	}
+    // 判断是否为空文档
+    if (this->config.size() == 0) {
+        // 重置为默认配置
+        resumeDefault();
+    }
 }
 
-void AppConfig::applyChange(const ConfigValue config)
+void AppConfig::applyChange(ConfigValue config)
 {
-	this->config = config;
+    this->config = config;
 }
 
 ConfigValue config::AppConfig::resumeDefault()
 {
-	///TODO: 使用流来 编写构建器函数来 定义toml配置文件
-	return NULL;
+    ///TODO: 使用流来 编写构建器函数来 定义toml配置文件
+    return NULL;
 }
 
-ConfigValue AppConfig::getConfig(const char * key) const
+ConfigValue AppConfig::getConfig(const char *key) const
 {
-	return find(this->config, key);
+    return find(this->config, key);
 }
 
-ConfigValue AppConfig::getConfig(const char * table, const char * key) const
+ConfigValue AppConfig::getConfig(const char *table, const char *key) const
 {
-	return getConfig(table).as_table().at(key);
+    return getConfig(table).as_table().at(key);
 }
 
-ConfigValue AppConfig::operator[](const char * key) const
+ConfigValue AppConfig::operator[](const char *key) const
 {
-	return this->getConfig(key);
+    return this->getConfig(key);
 }
 
 void AppConfig::writeToFile()
 {
-	ofstream stream(configFileName, ios::trunc);
-	stream << this->config.as_string();
-	stream.close();
+    ofstream stream(configFileName, ios::trunc);
+    stream << this->config.as_string();
+    stream.close();
 }
--- AppConfig/AppConfig.h
+++ AppConfig/AppConfig.h
@@ -1,101 +1,103 @@
-﻿#pragma once
-#include "pch.h"
+﻿#ifndef APPCONFIG_APPCONFIG_H
+#define APPCONFIG_APPCONFIG_H
 
 #include "toml/toml.h"
 #include <string>
 
 namespace config {
-	/// 文档配置项，用于获取配置项使用
-	using ConfigValue = const toml::value&;
+    /// 文档配置项，用于获取配置项使用
+    using ConfigValue = const toml::value &;
 
-	/**
-	 * @brief 程序配置类，读取toml文件中的配置
-	 *
-	 * @author nullptr
-	 * @version 1.0 2020-7-9
-	 * @version 1.1 2020-8-15 修改底层实现为toml格式文档
-	 * @since 1.0 2020-7-9
-	 */
-	class AppConfig
-	{
-	public:
-		/**
-		 * @brief 构造函数
-		 *
-		 * @param configFileName 配置文件名称
-		 */
-		AppConfig(const char* configFileName = "config.toml");
+    /**
+     * @brief 程序配置类，读取toml文件中的配置
+     *
+     * @author nullptr
+     * @version 1.0 2020-7-9
+     * @version 1.1 2020-8-15 修改底层实现为toml格式文档
+     * @since 1.0 2020-7-9
+     */
+    class AppConfig
+    {
+    public:
+        /**
+         * @brief 构造函数
+         *
+         * @param configFileName 配置文件名称
+         */
+        AppConfig(const char *configFileName = "config.toml");
 
-		/**
-		 * @brief 获取文档副本
-		 *
-		 * @since 1.0
-		 * @deprecated
-		 */
-		inline toml::value getConfig() const { return this->config; }
-		
-		/**
-		 * @brief 应用对文档的更改
-		 *
-		 * @param config 新文档
-		 * @since 1.0
-		 * @deprecated
-		 */
-		void applyChange(const ConfigValue config);
-		
-		/**
-		 * @brief 重置整个文档为默认值
-		 *
-		 * @return 重置后的文档副本
-		 * @since 1.0
-		 * @deprecated
-		 */
-		ConfigValue resumeDefault();
-		
-		/**
-		 * @brief 根据键名来获取配置项
-		 *
-		 * @param key 配置项键名
-		 * @return 配置项键名对应的值，调用其as_xxx()可以获取到具体的值，xxx为类型名称，如as_string()
-		 * @since 1.0
-		 * 
-		 */
-		ConfigValue getConfig (const char* key) const;
-		
-		/**
-		 * @brief 根据表名和键名来获取配置项
-		 *
-		 * @param table 配置项表名
-		 * @param key 配置项键名
-		 * @return 配置项键名对应的值，调用其as_xxx()可以获取到具体的值，xxx为类型名称，如as_string()
-		 * @since 1.0
-		 * 
-		 */
-		ConfigValue getConfig(const char* table, const char* key) const;
-		
-		/**
-		 * @brief 下标运算符重载，用于获取文档中的对象值
-		 *
-		 * @param key 配置项键名
-		 * @return 配置项对应的值，调用其as_xxx()可以获取到具体的值，xxx为类型名称，如as_string()
-		 * @since 1.0
-		 * 
-		 */
-		ConfigValue operator[](const char*  key) const;
+        /**
+         * @brief 获取文档副本
+         *
+         * @since 1.0
+         * @deprecated
+         */
+        inline toml::value getConfig() const { return this->config; }
 
-	private:
-		/**
-		 * @brief 将文档写入文件之中
-		 *
-		 * @since 1.0
-		 * @deprecated
-		 */
-		void writeToFile();
+        /**
+         * @brief 应用对文档的更改
+         *
+         * @param config 新文档
+         * @since 1.0
+         * @deprecated
+         */
+        void applyChange(ConfigValue config);
 
-	private:
-		/// 文档对象
-		toml::value config;
-		/// 配置文件名
-		std::string configFileName;
-	};
+        /**
+         * @brief 重置整个文档为默认值
+         *
+         * @return 重置后的文档副本
+         * @since 1.0
+         * @deprecated
+         */
+        ConfigValue resumeDefault();
+
+        /**
+         * @brief 根据键名来获取配置项
+         *
+         * @param key 配置项键名
+         * @return 配置项键名对应的值，调用其as_xxx()可以获取到具体的值，xxx为类型名称，如as_string()
+         * @since 1.0
+         *
+         */
+        ConfigValue getConfig(const char *key) const;
+
+        /**
+         * @brief 根据表名和键名来获取配置项
+         *
+         * @param table 配置项表名
+         * @param key 配置项键名
+         * @return 配置项键名对应的值，调用其as_xxx()可以获取到具体的值，xxx为类型名称，如as_string()
+         * @since 1.0
+         *
+         */
+        ConfigValue getConfig(const char *table, const char *key) const;
+
+        /**
+         * @brief 下标运算符重载，用于获取文档中的对象值
+         *
+         * @param key 配置项键名
+         * @return 配置项对应的值，调用其as_xxx()可以获取到具体的值，xxx为类型名称，如as_string()
+         * @since 1.0
+         *
+         */
+        ConfigValue operator[](const char *key) const;
+
+    private:
+        /**
+         * @brief 将文档写入文件之中
+         *
+         * @since 1.0
+         * @deprecated
+         */
+        void writeToFile();
+
+    private:
+        /// 文档对象
+        toml::value config;
+        /// 配置文件名
+        std::string configFileName;
+    };
 }
+
+#endif
